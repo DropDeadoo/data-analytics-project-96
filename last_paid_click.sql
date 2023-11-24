e975e62f8bcc4ea90a1f5049b4107d34a8ff571d
Анастасия <68104221+DropDeadoo@users.noreply.github.com>
2023-11-24 14:14:35 +0300

    last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,43 +1,32 @@
 /* Витрина для модели атрибуции Last Paid Click */
-/* Создаем подзапрос, которые объединяет три поля из 2-х таблиц с рекламой от ВК и Яндекса */
-with vk_and_ya_utm as (
-    select
-        utm_source,
-        utm_medium,
-        utm_campaign
-    from vk_ads
-    union all
-    select
-        utm_source,
-        utm_medium,
-        utm_campaign
-    from ya_ads
-),
+
 /* Создаём подзапрос в котором находим необходимые поля по условиям + добавляем row_number в разрезе id пользователей */
-union_sessions as (
+with union_sessions as (
     select
         s.visitor_id,
         s.visit_date,
-        vkya.utm_source,
-        vkya.utm_medium,
-        vkya.utm_campaign,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
         l.status_id,
-        row_number() over (partition by s.visitor_id order by visit_date) as rw /* Нумеруем id пользователей, с сортировкой по совершившим последнюю покупку*/
+        row_number()
+            over (partition by s.visitor_id order by visit_date desc)
+        as rw
+        /* Нумеруем id пользователей, с сортировкой по совершившим последнюю покупку*/
     from sessions as s
     left join leads as l
-        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at /* соединяем по полю id пользователя и дате, чтобы отсеять даты посещен*/
-    left join vk_and_ya_utm as vkya
         on
-            s.source = vkya.utm_source
-            and s.medium = vkya.utm_medium
-            and s.campaign = vkya.utm_campaign
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
+    where
+        s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 )
 
-/*Пишем основной запрос в котором бёрем все поля их подзапроса union_sessions за исключением rw и фильтруем записи только с платными кликами и последними покупками пользователей */
+/*Пишем основной запрос в котором бёрем все поля их подзапроса union_sessions за исключением rw и фильтруем записи только с последними покупками пользователей */
 select
     visitor_id,
     visit_date,
@@ -52,7 +41,6 @@ select
 from union_sessions
 where
     rw = '1'
-    and utm_medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 order by
     amount desc nulls last,
     visit_date asc,
